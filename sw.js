30023c0ab5e62be6bded322812fec1b62f62030e
johanssonbvp <johansson.bvp@gmail.com>
2026-03-14 13:55:44 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'petanque-v7';
+
+const FILES = [
+  './',
+  './index.html',
+  './skjuttraning.html',
+  './laggtraning.html',
+  './tavlingssimulator.html',
+  './dagbok.html',
+  'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;800;900&family=Barlow:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200 && e.request.method === 'GET') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
