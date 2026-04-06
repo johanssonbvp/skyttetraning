cae870ab334907a953c1bef53b425039ef7bf8dd
johanssonbvp <johansson.bvp@gmail.com>
2026-04-06 14:25:41 +0200

    Update cache version to 'petanque-v37'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'petanque-v36';
+const CACHE = 'petanque-v37';
 
 const FILES = [
   './',
@@ -7,6 +7,7 @@ const FILES = [
   './laggtraning.html',
   './tavlingssimulator.html',
   './dagbok.html',
+  './precisionsskytte.html'
   'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;800;900&family=Barlow:wght@400;500;600;700&display=swap'
 ];
 
