b7ab18f1f9eaa9111be00f97e435e11bbb174b83
johanssonbvp <johansson.bvp@gmail.com>
2026-04-07 09:25:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'petanque-v38';
+const CACHE = 'petanque-v39';
 
 const FILES = [
   './',
@@ -7,7 +7,7 @@ const FILES = [
   './laggtraning.html',
   './tavlingssimulator.html',
   './dagbok.html',
-  './precisionsskytte.html'
+  './precisionsskytte.html',
   'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;800;900&family=Barlow:wght@400;500;600;700&display=swap'
 ];
 
