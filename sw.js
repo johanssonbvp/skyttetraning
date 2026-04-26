84ddccb18115b768a64f623f8e0b2e365466692b
johanssonbvp <johansson.bvp@gmail.com>
2026-04-26 07:52:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,6 +14,9 @@ const CORE_FILES = [
   './tavlingssimulator.html',
   './dagbok.html',
   './precisionsskytte.html',
+  './storage-manager.js',
+  './lang-manager.js',
+  './offline-indicator.js',
   './sw.js',
   'https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;600;700;800;900&family=Barlow:wght@400;500;600;700&display=swap'
 ];
